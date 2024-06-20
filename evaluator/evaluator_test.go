c95fdccff8c1587b369470aba2fb006994ac59e4
Nitin <nitinmax1000@gmail.com>
2024-06-21 02:26:45 +0530

    add evaluate infix expression for +, -, * and /

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -13,7 +13,20 @@ func TestEvalIntegerExpression(t *testing.T) {
 		expected int64
 	}{
 		{"5", 5},
-		{"224", 224},
+		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"-50 + 100 + -50", 0},
+		{"5 * 2 + 10", 20},
+		{"5 + 2 * 10", 25},
+		{"20 + 2 * -10", 0},
+		{"50 / 2 * 2 + 10", 60},
+		{"2 * (5 + 10)", 30},
+		{"3 * 3 * 3 + 10", 37},
+		{"3 * (3 * 3) + 10", 37},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
 	}
 
 	for _, tt := range tests {
@@ -38,22 +51,22 @@ func TestEvalBooleanExpression(t *testing.T) {
 }
 
 func TestBangOperator(t *testing.T) {
-    tests := []struct {
-        input string
-        expected bool
-    } {
-        {"!true", false},
-        {"!false", true},
-        {"!5", false},
-        {"!!true", true},
-        {"!!false", false},
-        {"!!5", true},
-    }
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
 
-    for _, tt := range tests {
-        evaluated := testEval(tt.input)
-        testBooleanObject(t, evaluated, tt.expected)
-    }
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
 }
 
 func testEval(input string) object.Object {
