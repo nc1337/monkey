bc494e664c4b722694cfc697060cdafa68ccdd28
Nitin <nitinmax1000@gmail.com>
2024-06-21 02:14:37 +0530

    supported boolean and ! prefix operator

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -22,24 +22,72 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
-func testEval(input string) object.Object {
-    l := lexer.New(input)
-    p := parser.New(l)
-    program := p.ParseProgram()
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
 
-    return Eval(program)
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestBangOperator(t *testing.T) {
+    tests := []struct {
+        input string
+        expected bool
+    } {
+        {"!true", false},
+        {"!false", true},
+        {"!5", false},
+        {"!!true", true},
+        {"!!false", false},
+        {"!!5", true},
+    }
+
+    for _, tt := range tests {
+        evaluated := testEval(tt.input)
+        testBooleanObject(t, evaluated, tt.expected)
+    }
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
 }
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
-    result, ok := obj.(*object.Integer)
-    if !ok {
-        t.Errorf("object is not an integer. got=%T (%+v)", obj, obj )
-        return false
-    }
-    if result.Value != expected {
-        t.Errorf("object has wrong value. got=%d, want %d", 
-            result.Value, expected)
-        return false
-    }
-    return true
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not an integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want %d",
+			result.Value, expected)
+		return false
+	}
+	return true
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not an Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want %t",
+			result.Value, expected)
+		return false
+	}
+	return true
 }
